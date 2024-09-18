a003f97bd7d2aacc9bb73973e594c599f9807fea
Gideon Kofi <kofi>
2024-09-18 23:29:14 +0930

    the home page

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
         <Route path="/watch-video/:competitionId" element={<VideoWatch/>} />
         <Route path="/discovery-page" element={<DiscoveryPage/>} />
         <Route path="/menu" element={<Menu/>} />
-        <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
       </Routes>
       <Toaster />
     </Router>
