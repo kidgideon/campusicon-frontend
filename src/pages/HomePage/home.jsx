87691571a123b0b2c7707dfcb7265dc8db02ab38
Gideon Kofi <kofi>
2024-09-19 23:30:16 +0930

    seeing updates

--- src/pages/HomePage/home.jsx
+++ src/pages/HomePage/home.jsx
@@ -1,11 +1,24 @@
-const Home = () => {
+import { Link } from 'react-router-dom';
 
-      return(
-        <div className="home-page-interface">
-            <h1>coming soon</h1>
-        </div>
-      )
+const Home = () => {
+  return (
+    <div className="home-page-interface">
+      <h1>Coming Soon</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Home;
-
